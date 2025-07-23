7c9f8d879fe2631c43639308ad751bd6a452e6f3
Francisco <fhernandezr42@gmail.com>
2025-07-23 16:55:48 +0200

    file testing functions

--- test_data_fetcher_web_generator.py
+++ test_data_fetcher_web_generator.py
@@ -1,10 +1,17 @@
 import requests
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
 def main():
     """Proof of concept, connection for APY-Ninjas"""
     name = input("I need information about (enter animal name): ")
     api_url = f'https://api.api-ninjas.com/v1/animals?name={name}'
-    response = requests.get(api_url, headers={'X-Api-Key': 'gbhG5zXlusHL4o5dgJ7RxQ==OYecyDBtpCoL1PCG'})
+    response = requests.get(api_url, headers={'X-Api-Key': f"{API_KEY}"})
     if response.status_code != 200:
         print(f"Error: {response.status_code}")
         return
@@ -18,10 +25,6 @@ def main():
         print("-" * 50)
         print(f"{animal['name']}")
         print(f"Location: {animal['locations']}")
-        print(f"Type: {animal['taxonomy']['class']}")
-        print(f"Scientific name: {animal['taxonomy']['scientific_name']}")
-        print(f"Diet: {animal['characteristics']['diet']}")
-        print(f"Weight: {animal['characteristics']['weight']}")
         print("-"*50)
 
 
