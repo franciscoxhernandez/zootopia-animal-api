5f65d26699e812949814222e399f93de6348e1e5
Francisco <fhernandezr42@gmail.com>
2025-07-23 16:14:49 +0200

    Define API_KEY as a constant

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,9 +1,11 @@
 import requests
 
+API_KEY = 'gbhG5zXlusHL4o5dgJ7RxQ==OYecyDBtpCoL1PCG'
+
 def get_animal_data(animal_name):
     """This function gets animal data from API Ninjas"""
     api_url = f'https://api.api-ninjas.com/v1/animals?name={animal_name}'
-    response = requests.get(api_url, headers={'X-Api-Key': 'gbhG5zXlusHL4o5dgJ7RxQ==OYecyDBtpCoL1PCG'})
+    response = requests.get(api_url, headers={'X-Api-Key': f"{API_KEY}"})
 
     if response.status_code != 200:
         print(f"Error fetching data: {response.status_code}")
